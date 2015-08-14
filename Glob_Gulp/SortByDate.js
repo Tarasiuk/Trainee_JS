d1b19d46a8ddaa287cab4ec7408826118c387149
Anastasiya Tarasyuk <kapitalina@tut.by>
2015-08-14 17:55:08 +0300

    prefix

--- Glob_Gulp/SortByDate.js
+++ Glob_Gulp/SortByDate.js
@@ -1,18 +1,17 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
 var glob = require("glob");
+var util = require("gulp-util");
 var argv = require("yargs").argv;
 var rename = require("gulp-rename");
 
 gulp.task('parseFolders', function(){
+    var count = 0;
+   // util.log(count);
     return gulp.src(
         './Test/**/*.jpg'
-    ).pipe(rename(function(path){
-                gutil.log(path.basename);
-                //console.log("Hello, I'm working...somehow");
-                path.basename += "hello";
-                gutil.log(path.basename);
-                path.extname = ".jpg";
-            }
-        ));
+    ).pipe(rename(function (path) {
+            //util.log(count);
+            path.basename = count++ +"_" + path.basename;
+        }))
+    .pipe(gulp.dest("./new"));
 });
